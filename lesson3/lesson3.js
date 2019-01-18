4461bc539697745ec2a73c6247ae65d00f5ce8e3
forkis <roman.lats.pro@gmail.com>
2019-01-18 23:46:03 +0700

    задания 3 и 4

---
+++ lesson3/lesson3.js
@@ -0,0 +1,18 @@
+// Задание №1
+
+let a = 0;
+while (a <= 100) {
+  alert(a++);
+}
+
+// Задание №2
+
+// Задание №3
+
+for(let i = 0; i <= 9; i++)
+alert(i);
+
+// Задание №4
+
+for(let x = 'x'; x != 'xxxxxxxxxxxxxxxxxxxxx'; x = x + 'x')
+console.log(x);
